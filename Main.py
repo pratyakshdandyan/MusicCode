7789d8076bc15b6e841f28bee6ccde0224fd23ad
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2018-12-28 17:57:59 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import asyncio
 from itertools import cycle
 import os
 
-bot = commands.bot(command_prefix = "m.")
+bot = commands.Bot(command_prefix=("m."))
 status = ["testing the bot", "m.help"]
 
 async def change_status():
