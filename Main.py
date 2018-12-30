7c2ee652b8398348f5ca2ceff809553d46f3a099
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2018-12-29 20:04:35 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,42 +5,43 @@ import time
 from itertools import cycle
 import os
 
-bot = commands.Bot(command_prefix=("m."))
+client = commands.Bot(command_prefix=("m."))
 status = ["testing the bot", "m.help"]
 
 async def change_status():
-  await bot.wait_until_ready()
+  await client.wait_until_ready()
   msgs = cycle(status)
   
-  while not bot.is_closed:
+  while not client.is_closed:
     current_status = next(msgs)
-    await bot.change_presence(game=discord.Game(name=current_status))
+    await client.change_presence(game=discord.Game(name=current_status))
     await asyncio.sleep(5)
     
-@bot.event
+@client.event
 async def on_ready():
 	print('Logged in as')
-	print("User name:", bot.user.name)
-	print("User id:", bot.user.id)
+	print("User name:", client.user.name)
+	print("User id:", client.user.id)
 	print('---------------')
     
-@bot.command(pass_context=True)
+@client.command(pass_context=True)
 async def ping(ctx):
     """Pings the bot and gets a response time."""
     pingtime = time.time()
     pingms = await bot.say("Pinging...")
     ping = (time.time() - pingtime) * 1000
-    await bot.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
+    await client.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
     
-@bot.command(pass_context=True)
+@client.command(pass_context=True)
 async def join(ctx):
 	channel = ctx.message.author.voice.voice_channel
-	await bot.join_voice_channel(channel)
+	await client.join_voice_channel(channel)
 	
-@bot.command(pass_context=True)
-async def leave(con):
-	if bot.is_voice_connected(con.message.server) == True:
-		await bot.voice_client_in(con.message.server).disconnect()
+@client.command(pass_context=True)
+async def leave(ctx):
+	server = ctx.message.server
+	voice_client = client.voice_client_in(server)
+	await voice_client.disconnect()
 	
-bot.loop.create_task(change_status())
-bot.run(os.environ['BOT_TOKEN'])
+client.loop.create_task(change_status())
+client.run(os.environ['BOT_TOKEN'])
