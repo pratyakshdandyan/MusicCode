79ddca13270f9b29b8934c749f4e0cef482ec533
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2018-12-30 18:04:33 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,17 +12,16 @@ OPUS_LIBS = ["libopus-0.x86.dll","libopus-0.x64.dll","libopus-0.dll","libopus.so
 def load_opus_lib(opus_libs=OPUS_LIBS):
 	if opus.is.loaded():
 		return True:
-			
+	
 	for opus_libs in opus_libs:
 		try:
 			opus.load_opus(opus_lib)
 			return
 		except OSError:
 			pass
-			
+		
 raise RuntimeError('Could not load an opus lib. Tried %s' %)
 (', '.join(opus_libs)))
-
 
 
 
