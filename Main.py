8519da614f24d63c44c75458ca31649ff7b95dfa
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2019-01-01 01:21:23 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -35,26 +35,26 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.content.startswith('m.play '):
-      author = message.author
-      name = message.content.replace("m.play ", '')                 
-      fullcontent = ('http://www.youtube.com/results?search_query=' + name)
-      text = requests.get(fullcontent).text
-      soup = bs4.BeautifulSoup(text, 'html.parser')
-      img = soup.find_all('img')
-      div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
-      a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
-      title = (a[0]['title'])
-      a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
-      url = ('http://www.youtube.com'+a0['href'])
-      delmsg = await client.send_message(message.channel, 'Now Playing ** >> ' + title + '**')
-      server = message.server
-      voice_client = client.voice_client_in(server)
-      player = await voice_client.create_ytdl_player(url)
-      players[server.id] = player
-      print("User: {} From Server: {} is playing {}".format(author, server, title))
-      player.start()
-  await client.process_commands(message)
+	if message.content.startswith('m.play '):
+		author = message.author
+		name = message.content.replace("m.play ", '')                 
+		fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+		text = requests.get(fullcontent).text
+		soup = bs4.BeautifulSoup(text, 'html.parser')
+		img = soup.find_all('img')
+		div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+		a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+		title = (a[0]['title'])
+		a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+		url = ('http://www.youtube.com'+a0['href'])
+		delmsg = await client.send_message(message.channel, 'Now Playing ** >> ' + title + '**')
+		server = message.server
+		voice_client = client.voice_client_in(server)
+		player = await voice_client.create_ytdl_player(url)
+		players[server.id] = player
+		print("User: {} From Server: {} is playing {}".format(author, server, title))
+		player.start()
+	await client.process_commands(message)
 
 @client.command(pass_context=True)
 async def ping(ctx):
