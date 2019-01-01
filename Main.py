9ccd657e9cb76e5e9d5ae9cefc2d11142e11a270
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2019-01-01 01:44:18 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,9 +4,7 @@ from discord.ext import commands
 import asyncio
 import time
 import random
-from discord import Game
 from itertools import cycle
-import json
 import os
 import bs4, requests
 from time import gmtime, strftime
@@ -14,7 +12,17 @@ from discord import opus
 import youtube_dl
 
 client = commands.Bot(command_prefix=("m."))
+status = ["testing the bot", "m.help"]
 
+async def change_status():
+  await client.wait_until_ready()
+  msgs = cycle(status)
+  
+  while not client.is_closed:
+    current_status = next(msgs)
+    await client.change_presence(game=discord.Game(name=current_status))
+    await asyncio.sleep(5)
+    
 players = {}	
 
 @client.event
@@ -23,30 +31,39 @@ async def on_ready():
     print("User name:", client.user.name)
     print("User id:", client.user.id)
     print('---------------')
-
 @client.event
 async def on_message(message):
-	if message.content.startswith('m.play '):
-		author = message.author
-		name = message.content.replace("m.play ", '')                 
-		fullcontent = ('http://www.youtube.com/results?search_query=' + name)
-		text = requests.get(fullcontent).text
-		soup = bs4.BeautifulSoup(text, 'html.parser')
-		img = soup.find_all('img')
-		div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
-		a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
-		title = (a[0]['title'])
-		a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
-		url = ('http://www.youtube.com'+a0['href'])
-		delmsg = await client.send_message(message.channel, 'Now Playing ** >> ' + title + '**')
-		server = message.server
-		voice_client = client.voice_client_in(server)
-		player = await voice_client.create_ytdl_player(url)
-		players[server.id] = player
-		print("User: {} From Server: {} is playing {}".format(author, server, title))
-		player.start()
-	await client.process_commands(message)
-
+  if message.content == 'm.skip':
+      serverid = message.server.id
+      players[serverid].stop()
+  if message.content == 'm.pause':
+      serverid = message.server.id
+      players[serverid].pause()
+      await client.send_message(message.channel, "Player paused")
+  if message.content == 'm.resume':
+      serverid = message.server.id
+      players[serverid].resume()
+      await client.send_message(message.channel, "Player resumed")
+  if message.content.startswith('m.play '):
+      author = message.author
+      name = message.content.replace("m.play ", '')                 
+      fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+      text = requests.get(fullcontent).text
+      soup = bs4.BeautifulSoup(text, 'html.parser')
+      img = soup.find_all('img')
+      div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+      a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+      title = (a[0]['title'])
+      a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+      url = ('http://www.youtube.com'+a0['href'])
+      delmsg = await client.send_message(message.channel, 'Now Playing ** >> ' + title + '**')
+      server = message.server
+      voice_client = client.voice_client_in(server)
+      player = await voice_client.create_ytdl_player(url)
+      players[server.id] = player
+      print("User: {} From Server: {} is playing {}".format(author, server, title))
+      player.start()
+  await client.process_commands(message)
 @client.command(pass_context=True)
 async def ping(ctx):
     pingtime = time.time()
@@ -66,23 +83,5 @@ async def leave(ctx):
     voice_client = client.voice_client_in(server)
     await voice_client.disconnect()
     await client.say('Left voice channel')
-	
-@client.command(pass_context=True)
-async def skip(ctx):
-	serverid = message.server.id
-	players[serverid].stop()
-	await client.say('music have been skip')
-	
-@client.command(pass_context=True)
-async def pause(ctx):
-	serverid = message.server.id
-	players[serverid].pause()
-	await client.say("Player paused")
-	
-@client.command(pass_context=True)
-async def resume(ctx):
-	serverid = message.server.id
-	players[serverid].resume()
-	await client.say("Player resumed")
 	
 client.run(os.environ['BOT_TOKEN'])
