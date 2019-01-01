a9772f7792041de12dc384650722b5d7600d5984
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2019-01-01 00:28:42 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,17 @@
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
-import youtube_dl
 import asyncio
 import time
+import random
+from discord import Game
 from itertools import cycle
+import json
 import os
+import bs4, requests
+from time import gmtime, strftime
+from discord import opus
+import youtube_dl
 
 client = commands.Bot(command_prefix=("m."))
 status = ["testing the bot", "m.help"]
@@ -26,6 +33,40 @@ async def on_ready():
 	print("User name:", client.user.name)
 	print("User id:", client.user.id)
 	print('---------------')
+	
+@client.event
+async def on_message(message):
+  if message.content == 'm.skip':
+      serverid = message.server.id
+      players[serverid].stop()
+  if message.content == 'm.pause':
+      serverid = message.server.id
+      players[serverid].pause()
+      await client.send_message(message.channel, "Player paused")
+  if message.content == 'm.resume':
+      serverid = message.server.id
+      players[serverid].resume()
+      await client.send_message(message.channel, "Player resumed")
+  if message.content.startswith('m.play '):
+      author = message.author
+      name = message.content.replace("m.play ", '')                 
+      fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+      text = requests.get(fullcontent).text
+      soup = bs4.BeautifulSoup(text, 'html.parser')
+      img = soup.find_all('img')
+      div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+      a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+      title = (a[0]['title'])
+      a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+      url = ('http://www.youtube.com'+a0['href'])
+      delmsg = await client.send_message(message.channel, 'Now Playing ** >> ' + title + '**')
+      server = message.server
+      voice_client = client.voice_client_in(server)
+      player = await voice_client.create_ytdl_player(url)
+      players[server.id] = player
+      print("User: {} From Server: {} is playing {}".format(author, server, title))
+      player.start()
+  await client.process_commands(message)
     
 @client.command(pass_context=True)
 async def ping(ctx):
@@ -39,20 +80,14 @@ async def ping(ctx):
 async def join(ctx):
 	channel = ctx.message.author.voice.voice_channel
 	await client.join_voice_channel(channel)
+	await client.say('Connected to voice channel: **[' + str(channel) + ']**')
 	
 @client.command(pass_context=True)
 async def leave(ctx):
 	server = ctx.message.server
 	voice_client = client.voice_client_in(server)
 	await voice_client.disconnect()
-	
-@client.command(pass_context=True)
-async def play(ctx, url):
-	server = ctx.message.server
-	voice_client = client.voice_client_in(server)
-	player = await voice_client.create_ytdl_player(url)
-	player[server.id] = player
-	player.start()
+	await client.say('I left the voice channel')
 	
 client.loop.create_task(change_status())
 client.run(os.environ['BOT_TOKEN'])
