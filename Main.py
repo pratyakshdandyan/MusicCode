1e4260d3fafff5d7f6c40b050f95374f0d4bb63a
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2019-01-13 21:30:53 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -76,7 +76,7 @@ async def stop(ctx):
 @client.command(pass_context=True)
 async def play(ctx, *, name):
 	author = ctx.message.author
-	name = message.content.replace("m.play ", '')
+	name = ctx.message.content.replace("m.play ", '')
 	fullcontent = ('http://www.youtube.com/results?search_query=' + name)
 	text = requests.get(fullcontent).text
 	soup = bs4.BeautifulSoup(text, 'html.parser')
