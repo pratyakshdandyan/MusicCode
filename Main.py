5f0a8eab0e1db4b7b71862f2c25039b293fa4165
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2019-01-14 21:43:01 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -76,7 +76,7 @@ async def _play(ctx, *, name):
 	title = (a[0]['title'])
 	a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
 	url = ('http://www.youtube.com'+a0['href'])
-	delmsg = await client.send_message(message.channel, 'Searching... ** >> ' + title + '**')
+	delmsg = await client.say(message.channel, 'Searching... ** >> ' + title + '**')
 	server = ctx.message.server
 	voice_client = client.voice_client_in(server)
 	player = await voice_client.create_ytdl_player(url)
