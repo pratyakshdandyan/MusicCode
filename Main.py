1b6e8070afe1308e401f1ac308b8f91a0b4a7108
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2019-01-24 15:39:24 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -149,14 +149,13 @@ async def support(ctx):
 	embed = discord.Embed(color=user.colour)
 	embed.add_field(name="Support server", value=f"[Link](https://discord.gg/ccAuKgV)")
 	embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/489033991769423873/5215f2354e333ef5ca21124d45f70efd.png?size=1024")
-	embed.set_footer(text=" | {}".format(client.user.name), icon_url="https://cdn.discordapp.com/avatars/489033991769423873/5215f2354e333ef5ca21124d45f70efd.png?size=1024")
 	await client.say(embed=embed)
 	
 @client.command(pass_context=True)
 async def invite(ctx):
 	user = ctx.message.author
 	embed = discord.Embed(color=user.colour)
-	embed.add_field(name="Bot Link", value=f"[Link](https://discordapp.com/api/oauth2/authorize?client_id=489033991769423873&permissions=36784384&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D489033991769423873%26permissions%3D36784384%26redirect_uri%3Dhttps%253A%252F%252Fdiscordapp.com%252Fapi%252Foauth2%252Fauthorize%253Fclien&scope=bot)")
+	embed.add_field(name="Bot Link", value=f"[Link](https://discordapp.com/api/oauth2/authorize?client_id=489033991769423873&permissions=36784128&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D489033991769423873%26permissions%3D36782080%26redirect_uri%3Dhttps%253A%252F%252Fdiscordapp.com%252Fapi%252Foauth2%252Fauthorize%253Fclien&scope=bot)")
 	await client.say(embed=embed)
 	
 @client.command(pass_context=True)
